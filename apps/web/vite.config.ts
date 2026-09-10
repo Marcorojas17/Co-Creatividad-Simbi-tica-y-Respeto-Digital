580f720c7deae6491d3bc1b59314731291bae52e
Marcorojas17 <86907529+Marcorojas17@users.noreply.github.com>
2026-09-10 22:51:40 +0000

    fix: add terser dep - build platinum ready

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,10 +5,9 @@ export default defineConfig({
   build: {
     minify: 'terser',
     terserOptions: {
-      compress: { drop_console: true, drop_debugger: true, passes: 3 },
+      compress: { drop_console: true, drop_debugger: true, passes: 2 },
       mangle: true,
       format: { comments: false }
-    },
-    chunkSizeWarningLimit: 500
+    }
   }
 })
